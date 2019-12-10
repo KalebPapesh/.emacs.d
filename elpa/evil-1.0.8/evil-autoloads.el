4ff8d9bdac550900fc5baa430a4602bbf728f497
Kaleb Papesh <kaleb.papesh@gmail.com>
2019-12-10 13:05:04 -0800

    adding back in elpa for packages needed

---
+++ elpa/evil-1.0.8/evil-autoloads.el
@@ -0,0 +1,91 @@
+;;; evil-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "evil-commands" "evil-commands.el" (0 0 0 0))
+;;; Generated autoloads from evil-commands.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-commands" '("evil-")))
+
+;;;***
+
+;;;### (autoloads nil "evil-common" "evil-common.el" (0 0 0 0))
+;;; Generated autoloads from evil-common.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-common" '("evil-")))
+
+;;;***
+
+;;;### (autoloads nil "evil-core" "evil-core.el" (0 0 0 0))
+;;; Generated autoloads from evil-core.el
+ (autoload 'evil-mode "evil" "Toggle evil in all buffers" t)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-core" '("evil-" "turn-o")))
+
+;;;***
+
+;;;### (autoloads nil "evil-digraphs" "evil-digraphs.el" (0 0 0 0))
+;;; Generated autoloads from evil-digraphs.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-digraphs" '("evil-digraph")))
+
+;;;***
+
+;;;### (autoloads nil "evil-ex" "evil-ex.el" (0 0 0 0))
+;;; Generated autoloads from evil-ex.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-ex" '("evil-")))
+
+;;;***
+
+;;;### (autoloads nil "evil-macros" "evil-macros.el" (0 0 0 0))
+;;; Generated autoloads from evil-macros.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-macros" '("evil-")))
+
+;;;***
+
+;;;### (autoloads nil "evil-repeat" "evil-repeat.el" (0 0 0 0))
+;;; Generated autoloads from evil-repeat.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-repeat" '("evil-")))
+
+;;;***
+
+;;;### (autoloads nil "evil-search" "evil-search.el" (0 0 0 0))
+;;; Generated autoloads from evil-search.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-search" '("evil-")))
+
+;;;***
+
+;;;### (autoloads nil "evil-states" "evil-states.el" (0 0 0 0))
+;;; Generated autoloads from evil-states.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-states" '("evil-")))
+
+;;;***
+
+;;;### (autoloads nil "evil-vars" "evil-vars.el" (0 0 0 0))
+;;; Generated autoloads from evil-vars.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-vars" '("evil-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("evil-integration.el" "evil-maps.el" "evil-pkg.el"
+;;;;;;  "evil-types.el" "evil.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; evil-autoloads.el ends here
---
+++ elpa/mac-key-mode-2010.1.3/mac-key-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mac-key-mode" "2010.1.3" "provide mac-style key bindings on Carbon Emacs" 'nil)
